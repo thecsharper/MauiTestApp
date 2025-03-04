b9955418b08b4627e7e6155a7cfbc701ad79a9dd
Simon Jones <si@thecsharper.com>
2025-03-04 08:04:28 +0000

    Added input entry element.

--- MauiTestApp/MainPage.xaml.cs
+++ MauiTestApp/MainPage.xaml.cs
@@ -20,6 +20,20 @@
 
             SemanticScreenReader.Announce(CounterBtn.Text);
         }
+
+
+        private void OnCounterEntryCompleted(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+        
     }
 
 }
