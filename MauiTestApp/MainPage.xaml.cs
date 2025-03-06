91d43e770c26fc5d0c084caeb8f735c063d63506
Simon Jones <si@thecsharper.com>
2025-03-06 09:00:48 +0000

    Added camera media picker

--- MauiTestApp/MainPage.xaml.cs
+++ MauiTestApp/MainPage.xaml.cs
@@ -33,7 +33,25 @@
 
             SemanticScreenReader.Announce(CounterBtn.Text);
         }
-        
-    }
 
+        // From: https://www.telerik.com/blogs/beyond-basics-getting-started-media-picker-net-maui
+        private async void TakePhoto(object sender, EventArgs e)
+        {
+            var photo = await MediaPicker.Default.PickPhotoAsync(new MediaPickerOptions
+            {
+                Title = "Select your photo"
+            });
+
+            if (photo != null)
+            {
+                // Getting the file result information
+                using Stream sourceStream = await photo.OpenReadAsync();
+
+                // Saving the file in your local storage
+                string localFilePath = Path.Combine(FileSystem.CacheDirectory, photo.FileName);
+
+                await sourceStream.CopyToAsync(File.OpenWrite(localFilePath));
+            }
+        }
+    }
 }
