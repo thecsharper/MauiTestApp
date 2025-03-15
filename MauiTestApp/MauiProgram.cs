2dc85c81af7450311befa19826a4f29fc833e429
Simon Jones <si@thecsharper.com>
2025-03-15 08:13:25 +0000

    Added Syncfusion package

--- MauiTestApp/MauiProgram.cs
+++ MauiTestApp/MauiProgram.cs
@@ -2,6 +2,9 @@
 
 namespace MauiTestApp;
 
+
+// Implement this: https://www.syncfusion.com/blogs/post/sleep-patterns-with-net-maui-toolkit?utm_source=alvinashcraft&utm_medium=email&utm_campaign=alvinashcraft_blog_edmmar25
+
 public static class MauiProgram
 {
 	public static MauiApp CreateMauiApp()
